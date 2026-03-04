dd499195ce709fc5555e03d7a21c617e680ceec9
Christina Kaddouh <christinakaddouh@gmail.com>
2026-03-04 10:08:45 +0100

    Add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/leaderboard">Leaderboard</RouterLink>
+    </nav>
     <RouterView />
   </div>
 </template>
@@ -17,6 +21,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  background: #1a1a2e;
+}
+
+nav a {
+  color: #e0e0e0;
+  text-decoration: none;
+}
+
+nav a.router-link-active {
+  color: #f5c518;
+  font-weight: bold;
 }
 </style>

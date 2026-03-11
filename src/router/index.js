a2b2c49469fb1e5bfa4db2612ad908b67ffbbc09
Christina Kaddouh <christinakaddouh@gmail.com>
2026-03-11 09:47:13 +0100

    Update nav guard to use store

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useGameStore } from '../stores/gameStore.js'
 import HomeView from '../views/HomeView.vue'
 import PlayView from '../views/PlayView.vue'
 import LeaderboardView from '../views/LeaderboardView.vue'
-import { useGameStore } from '../stores/gameStore.js'
-
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
@@ -16,14 +15,13 @@ const router = createRouter({
   routes
 })
 
-// TODO Week 7: replace sessionStorage check with store.gameStarted
-
 router.beforeEach((to) => {
-  const store = useGameStore()
-  if (to.name === 'play' && store.gameState !== 'playing') {
-    return { name: 'home' }
+  if (to.name === 'play') {
+    const store = useGameStore()
+    if (store.gameState !== 'playing') {
+      return { name: 'home' }
+    }
   }
 })
-  
+
 export default router
-  
